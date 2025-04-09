91f025ae1b4473592073868baa102b2cb5ef0d96
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-09 10:21:58 +0500

    refactor app setup to include dotenv configuration and user routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
-import express from 'express'
+import express from "express";
+import userRoutes from "./routes/user.route.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
-const app=express()
+dotenv.config();
 
-const PORT=3000
+const app = express();
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on localhost ${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/v1/user", userRoutes);
+app.listen(PORT, () => {
+  console.log(`Server is running on localhost ${PORT}`);
+  connectDB();
+});
