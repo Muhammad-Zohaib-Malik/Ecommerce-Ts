a385a0f6a7ecc9b4dc6065ac8f49f4f84a9da7ac
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-09 09:40:38 +0500

    added user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcrypt";
 
 interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   role: "admin" | "user";
+  cartItems: Array<{
+    quantity: number;
+    product: mongoose.Types.ObjectId;
+  }>;
+  comparePassword(candidatePassword: string): Promise<boolean>;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -44,10 +50,37 @@ const userSchema = new mongoose.Schema(
           "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character",
       },
     },
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model<IUser>("User", userSchema);
