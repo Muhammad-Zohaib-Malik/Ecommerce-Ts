8c30b99f54166da47770be0c3dc2fcbdedc95eec
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-13 23:10:35 +0500

    added AWS S3

--- backend/src/config/awsConfig.ts
+++ backend/src/config/awsConfig.ts
@@ -1,4 +1,6 @@
 import {S3Client} from '@aws-sdk/client-s3';
+import { Upload } from "@aws-sdk/lib-storage";
+
 
 export const s3Client = new S3Client({
     region: process.env.AWS_REGION,
@@ -7,3 +9,35 @@ export const s3Client = new S3Client({
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
     }
 })
+
+
+export const uploadPhotos = async (files: Express.Multer.File[]) => {
+  const photoData = await Promise.all(
+    files.map(async (file) => {
+      const key = `products/${Date.now()}-${file.originalname}`;
+
+      const uploadParams = {
+        Bucket: process.env.S3_BUCKET_NAME!,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      };
+
+      const uploadResult = await new Upload({
+        client: s3Client,
+        params: uploadParams,
+      }).done();
+
+      if (uploadResult.$metadata.httpStatusCode !== 200) {
+        throw new Error("Failed to upload image to S3");
+      }
+
+      return {
+        url: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`,
+        key: key,
+      };
+    })
+  );
+
+  return photoData; 
+};
