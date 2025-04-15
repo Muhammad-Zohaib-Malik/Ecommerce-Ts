48688f7e591cabda5dc916855f478bc575cf3cdd
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-15 17:51:35 +0500

    add product routes to the application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/user.route.js";
+import productRoutes from "./routes/product.route.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/products", productRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on localhost ${PORT}`);
   connectDB();
